e687e6db821cbe6be686cea41caf999231ef092c
Brenner Costa <costabrenner1@gmail.com>
2024-05-13 23:12:46 -0300

    initial database settings / initial folder structure / Clek and Prisma downloaded into the project

--- portfolio_brenner/src/app/about/page.tsx
+++ portfolio_brenner/src/app/about/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          <h1>HomePage funcionando</h1>
+          <h1>About funcionando </h1>
     </main>
   );
 }
