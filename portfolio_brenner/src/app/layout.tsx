fa19cf64272432b07ae259c08ef55f2f90322e0e
Brenner Costa <costabrenner1@gmail.com>
2024-05-20 19:59:20 -0300

    navbar completed without styling

--- portfolio_brenner/src/app/layout.tsx
+++ portfolio_brenner/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import './globals.css';
+import { Providers } from './providers'
+import { Navbar } from '@/components/Navbar';
 
 export default function RootLayout({
   children,
@@ -10,17 +12,21 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body>
-          <header>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
-          <main>
-            {children}
-          </main>
+          <Providers>
+            <header>
+              <Navbar home={'Home'} about={'About'} projects={'Projects'} skills={'Skills'} contact={'Contact'} />
+              {/* <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn> */}
+            </header>
+            <main>
+              {children}
+            </main>
+
+          </Providers>
         </body>
       </html>
     </ClerkProvider>
---
+++ portfolio_brenner/src/components/Navbar/index.tsx
@@ -0,0 +1,55 @@
+// components/Navbar.tsx
+import { Flex, Link, HStack } from "@chakra-ui/react";
+import { SignInButton, SignOutButton, UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import NextLink from "next/link";
+import { useEffect, useState } from "react";
+
+interface NavbarProps {
+    home: string;
+    about: string;
+    projects: string;
+    skills: string;
+    contact: string;
+}
+
+export function Navbar({ home, about, projects, skills, contact }: NavbarProps) {
+
+    /*
+        Criar o state para verificar se o sign in está ativo ou não
+    */
+
+    return (
+        <Flex as="nav" align="center" justify="space-between" p={4} bg="red" color="white">
+            <HStack mr={10} bg="black" w="100%" justify="space-between">
+                <NextLink href="/">
+                    <Link fontWeight="bold">{home}</Link>
+                </NextLink>
+                <NextLink href="/about">
+                    <Link>{about}</Link>
+                </NextLink>
+                <NextLink href="/skills">
+                    <Link>{skills}</Link>
+                </NextLink>
+                <NextLink href="/projects">
+                    <Link>{projects}</Link>
+                </NextLink>
+                <NextLink href="/contact">
+                    <Link>{contact}</Link>
+                </NextLink>
+                {(
+                    <>
+                        <SignedIn>
+                            <UserButton />
+                        </SignedIn>
+                        <SignedOut>
+                            <SignInButton />
+                        </SignedOut>
+                        <SignedIn>
+                            <SignOutButton />
+                        </SignedIn>
+                    </>
+                )}
+            </HStack>
+        </Flex>
+    );
+};
